611d57d350e09594e61d0d514d32eec97fc3fca6
Matt Llewelyn <mjllewelyn@gmail.com>
2025-06-22 16:13:57 -0500

    Add bibleHierarchy

---
+++ bibleData.js
@@ -0,0 +1,122 @@
+data = { "items": [
+  {
+    "name": "Old Testament",
+    "description": "",
+    "icon": "fa-scroll",
+    "items": [
+      {
+        "name": "Pentateuch",
+        "description": "",
+        "icon": "fa-scroll-torah",
+        "items": [
+          {
+            "name": "Genesis",
+            "description": "",
+            "icon": "fa-star-of-david",
+            "items": [
+              {
+                "name": "Genesis 1-11",
+                "description": "",
+                "icon": "fa-star",
+                "items": [
+                  {
+                    "name": "Creation",
+                    "description": "",
+                    "icon": "fa-globe",
+                    "items": []
+                  },
+                  {
+                    "name": "Fall",
+                    "description": "",
+                    "icon": "fa-person-falling-burst",
+                    "items": []
+                  },
+                  {
+                    "name": "Flood",
+                    "description": "",
+                    "icon": "fa-water",
+                    "items": []
+                  },
+                  {
+                    "name": "Babel",
+                    "description": "",
+                    "icon": "fa-gopuram",
+                    "items": []
+                  }
+                ]
+              },
+              {
+                "name": "Genesis 12-25",
+                "description": "",
+                "icon": "fa-people-group",
+                "items": []
+              }
+            ]
+          },
+          {
+            "name": "Exodus",
+            "description": "",
+            "icon": "fa-mountain",
+            "items": []
+          },
+          {
+            "name": "Leviticus",
+            "description": "",
+            "icon": "fa-tent",
+            "items": []
+          },
+          {
+            "name": "Numbers",
+            "description": "",
+            "icon": "fa-shoe-prints",
+            "items": []
+          },
+          {
+            "name": "Deuteronomy",
+            "description": "",
+            "icon": "fa-table-columns",
+            "items": []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "New Testament",
+    "description": "",
+    "icon": "fa-scroll",
+    "items": [
+      {
+        "name": "Gospels",
+        "description": "",
+        "icon": "fa-church",
+        "items": [
+          {
+            "name": "Matthew",
+            "description": "",
+            "icon": "fa-crown",
+            "items": []
+          },
+          {
+            "name": "Mark",
+            "description": "",
+            "icon": "fa-cross",
+            "items": []
+          },
+          {
+            "name": "Luke",
+            "description": "",
+            "icon": "fa-timeline",
+            "items": []
+          },
+          {
+            "name": "John",
+            "description": "",
+            "icon": "fa-arrow-up",
+            "items": []
+          }
+        ]
+      }
+    ]
+  }
+]}
